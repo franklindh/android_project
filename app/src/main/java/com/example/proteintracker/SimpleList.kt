ab15aabd399e735505114c3cbef288183ad99fe9
franklindh <101926929+franklindh@users.noreply.github.com>
2022-10-01 21:26:20 +0700

    Tugas 4 Edit revisi

--- app/src/main/java/com/example/proteintracker/SimpleList.kt
+++ app/src/main/java/com/example/proteintracker/SimpleList.kt
@@ -2,8 +2,13 @@ package com.example.proteintracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Spinner
 import com.google.android.material.snackbar.Snackbar
 
 class SimpleList : AppCompatActivity() {
@@ -15,6 +20,7 @@ class SimpleList : AppCompatActivity() {
         "porttitor", "sodales", "pellentesque", "augue", "purus"
     )
     lateinit var lvSample : ListView
+    lateinit var spSampleList : Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +35,45 @@ class SimpleList : AppCompatActivity() {
                 Snackbar.LENGTH_LONG).show()
         }
 
+        spSampleList = findViewById(R.id.spinner)
+        spSampleList.adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item, items)
+        spSampleList.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view:
+            View, position: Int, id: Long) {
+                Snackbar.make(lvSample, "Anda memilih: ${items[position]}",
+                    Snackbar.LENGTH_LONG).show()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+        registerForContextMenu(lvSample)
+
+
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?,
+                                     menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_sample_list_view, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        var info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        when(item.itemId){
+            R.id.add -> {
+                Snackbar.make(lvSample,"Anda memilih: Add Item ke-${info.id}", Snackbar.LENGTH_LONG).show()
+            }
+            R.id.edit -> {
+                Snackbar.make(lvSample,"Anda memilih: Edit Item ke-${info.id}", Snackbar.LENGTH_LONG).show()
+            }
+            R.id.delete -> {
+                Snackbar.make(lvSample,"Anda memilih: Delete Item ke-${info.id}", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+        return super.onContextItemSelected(item)
     }
 }
